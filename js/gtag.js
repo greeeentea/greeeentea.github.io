9362f1619f2f002d9a77468c68c307ac90ead3ea
greeeentea <92647790+greeeentea@users.noreply.github.com>
2022-06-09 11:57:20 +0200

    Create gtag.js

---
+++ js/gtag.js
@@ -0,0 +1,8 @@
+ window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+
+        gtag("config", "G-TWKXK88T9X")
